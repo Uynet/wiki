1d1842131fa531b4421a29438a59cb4baa2cdebf
uynet <highsate@gmail.com>
2018-05-09 06:47:07 +0900

    EntityManager

---
+++ entityManager.js
@@ -0,0 +1,15 @@
+import Entity from "./entity.js";
+
+export default class EntityManager{
+  static Init(){
+    this.list =[]; 
+
+    let a = new Entity({x:10,y:10});
+    let b = new Entity({x:80,y:80});
+    this.Add(a);
+    this.Add(b);
+  }
+  static Add(e){
+    this.list.push(e);
+  }
+ }
--- game.js
+++ game.js
@@ -1,23 +1,19 @@
 import Drawer from "./drawer.js";
 import Entity from "./entity.js";
+import EntityManager from "./entityManager.js";
 
-let entityList = [];
 
 export default class Game{
   static Init(){            
     Drawer.Init();
+    EntityManager.Init();
+
     Game.Run();
-
-    let a = new Entity({x:10,y:10});
-    let b = new Entity({x:80,y:80});
-    entityList.push(a);
-    entityList.push(b);
-
   }
   static Run(){
     //毎フレーム実行される部分
     //Game.Update()更新
-    Drawer.Render(entityList);
+    Drawer.Render(EntityManager.list);
     requestAnimationFrame(Game.Run);
   }
 }
